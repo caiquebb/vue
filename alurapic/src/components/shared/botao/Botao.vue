22b480c8114f9482ecbd243d395a20f7fd8b82fa
Caique Benassi Bertolozzi <caique.bertolozzi@agv.com.br>
2018-12-18 12:05:36 -0200

    Initial commit

---
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,59 @@
+<template>
+  <button @click="disparaAcao()" class="botao" :class="estiloDoBotao" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script>
+export default {
+
+    props: {
+        
+        tipo: {
+            type: String,
+            required: true,
+        },
+        
+        rotulo: {
+            type: String,
+            required: true
+        },
+
+        confirmacao: Boolean,
+        
+        estilo: String
+    },
+
+    methods: {
+
+        disparaAcao() {
+            
+            if (this.confirmacao && !confirm('Confirma operação?')) {
+                    
+                return;
+            }
+
+            this.$emit('botaoAtivado');
+        }
+    },
+
+    computed: {
+
+        estiloDoBotao() {
+            
+            if (this.estilo == 'padrao' || !this.estilo) {
+                
+                return 'botao-padrao';
+            }
+
+            if (this.estilo == 'perigo') {
+                
+                return 'botao-perigo';
+            }
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+    @import './Botao.scss';
+</style>
---
+++ alurapic/src/routes.js
@@ -0,0 +1,34 @@
+import Home from './components/home/Home.vue';
+// import Cadastro from './components/cadastro/Cadastro.vue';
+// const Cadastro = () => System.import('./components/cadastro/Cadastro.vue');
+
+const Cadastro = resolve => require(['./components/cadastro/Cadastro.vue'], m => resolve(m.default));
+
+export const routes = [
+    {
+        path: '',
+        name: 'home',
+        component: Home,
+        titulo: 'Home',
+        menu: true
+    },
+    {
+        path: '/cadastro',
+        name: 'cadastro',
+        component: Cadastro,
+        titulo: 'Cadastro',
+        menu: true
+    },
+    {
+        path: '/cadastro/:id',
+        name: 'altera',
+        component: Cadastro,
+        titulo: 'Cadastro',
+        menu: false
+    },
+    {
+        path: '*',
+        component: Home,
+        menu: false
+    }
+];
